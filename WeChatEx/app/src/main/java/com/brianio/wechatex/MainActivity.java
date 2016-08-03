13970c6d6b6117d0bf04709d22cadd0b2f645e5c
brianio <leegzone@gmail.com>
2016-08-03 19:44:09 +0800

    添加了微信界面

--- WeChatEx/app/src/main/java/com/brianio/wechatex/MainActivity.java
+++ WeChatEx/app/src/main/java/com/brianio/wechatex/MainActivity.java
@@ -1,13 +1,63 @@
 package com.brianio.wechatex;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setOverFlowShowAlways();
+
+    }
+
+    //overflow一直显示
+    public void setOverFlowShowAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //overflow内的显示图标
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ WeChatEx/app/src/main/java/com/brianio/wechatex/PlusActionProvider.java
@@ -0,0 +1,74 @@
+package com.brianio.wechatex;
+
+import android.content.Context;
+import android.view.ActionProvider;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class PlusActionProvider extends ActionProvider {
+    /**
+     * Creates a new instance. ActionProvider classes should always implement a
+     * constructor that takes a single Context parameter for inflating from menu XML.
+     *
+     * @param context Context for accessing resources.
+     */
+    private Context context;
+    public PlusActionProvider(Context context) {
+        super(context);
+        this.context=context;
+    }
+
+    @Override
+    public View onCreateActionView() {
+        return null;
+    }
+
+    @Override
+    public void onPrepareSubMenu(SubMenu subMenu){
+        subMenu.clear();
+        subMenu.add("群聊").setIcon(R.mipmap.ofm_group_chat_icon).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+        subMenu.add("添加朋友").setIcon(R.mipmap.ofm_add_icon).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+        subMenu.add("视频聊天").setIcon(R.mipmap.ofm_video_icon).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+        subMenu.add("扫一扫").setIcon(R.mipmap.ofm_qrcode_icon).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+        subMenu.add("收付款").setIcon(R.mipmap.ofm_card_icon).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean hasSubMenu(){
+        return true;
+    }
+}
