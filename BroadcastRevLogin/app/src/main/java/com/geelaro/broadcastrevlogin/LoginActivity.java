161a8babfeb44224d3ae886fcc13c5370864e35e
geelaro <leegzone@gmail.com>
2017-04-08 22:06:25 +0800

    add Remember password by SharedPreferences

--- BroadcastRevLogin/app/src/main/java/com/geelaro/broadcastrevlogin/LoginActivity.java
+++ BroadcastRevLogin/app/src/main/java/com/geelaro/broadcastrevlogin/LoginActivity.java
@@ -1,52 +1,88 @@
 package com.geelaro.broadcastrevlogin;
 
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.geelaro.broadcastrevlogin.BaseActivity;
-import com.geelaro.broadcastrevlogin.R;
 
 /**
  * Created by LEE on 2017/4/7.
  */
 
 public class LoginActivity extends BaseActivity {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button btnLogin;
+    private CheckBox rememberPass;
+    private String account;
+    private String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         init();
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
+        boolean isRemember = preferences.getBoolean("remember_password", false);
+        if (isRemember) {
+            //把账户信息都保存在文本框里
+            String account = preferences.getString("account", "");
+            String password = preferences.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String account=accountEdit.getText().toString();
-                String password=passwordEdit.getText().toString();
+                account = accountEdit.getText().toString();
+                password = passwordEdit.getText().toString();
                 //如果用户名为“admin”，密码为“123456”，则登录成功
-                if(account.equals("admin")&&password.equals("123456")){
-                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                if (account.equals("admin") && password.equals("123456")) {
+                    //检查复选框是否选中，选中保存账户信息，否则不保存
+                    saveData(rememberPass.isChecked());
+                    //登录成功，跳到首页
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
-                }
-                else {
-                    Toast.makeText(LoginActivity.this,"Account or Password is invalid.",Toast.LENGTH_SHORT).show();
+                } else {
+                    //提示账户信息错误
+                    Toast.makeText(LoginActivity.this, "Account or Password is invalid.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
-    //初始化
-    private void init(){
-        accountEdit=(EditText)findViewById(R.id.account);
-        passwordEdit=(EditText)findViewById(R.id.password);
-        btnLogin=(Button)findViewById(R.id.btn_login);
 
+    //初始化
+    private void init() {
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        rememberPass = (CheckBox) findViewById(R.id.remember_password);
+
+    }
+
+    //save data
+    private void saveData(Boolean isChecked) {
+        if (isChecked) {
+            editor.putString("account", account);
+            editor.putString("password", password);
+            editor.putBoolean("remember_password", true);
+        } else {
+            editor.clear();
+        }
+
+        editor.commit();
     }
 }
